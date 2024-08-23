97eafc0941526ba0d31c2e760d18555261ef256f
hinoky4693 <roghk1204@naver.com>
2024-08-23 12:06:05 +0900

    interface public 통일성을 위해서 제거.

--- src/main/java/com/dowadream/customercare/service/NotificationService.java
+++ src/main/java/com/dowadream/customercare/service/NotificationService.java
@@ -5,9 +5,9 @@ import com.dowadream.customercare.entity.NotificationEntity;
 import java.util.List;
 
 public interface NotificationService {
-    public void svcNotificationInsert(NotificationEntity notiVO);                   // 공지사항 추가
-    public List<NotificationEntity> svcNotificationList();                          // 공지사항 리스트 조회
-    public NotificationEntity svcNotificationDetail(NotificationEntity notiVO);     // 공지사항 디테일 조회
-    public void svcNotificationUpdate(NotificationEntity notiVO);                   // 공지사항 수정
-    public void svcNotificationDelete(NotificationEntity notiVO);                   // 공지사항 삭제
+    void svcNotificationInsert(NotificationEntity notiVO);                   // 공지사항 추가
+    List<NotificationEntity> svcNotificationList();                          // 공지사항 리스트 조회
+    NotificationEntity svcNotificationDetail(NotificationEntity notiVO);     // 공지사항 디테일 조회
+    void svcNotificationUpdate(NotificationEntity notiVO);                   // 공지사항 수정
+    void svcNotificationDelete(NotificationEntity notiVO);                   // 공지사항 삭제
 }
